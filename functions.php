ce361f19554ba51319ef8dfdf7151be8046999b9
Lori <lori@Loris-MacBook-Pro.local>
2021-07-12 09:33:20 -0500

    php functions

---
+++ functions.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>functions</title>
+    </head>
+    <body>
+        <?php
+            
+            function saySomething() {
+                echo "Hello Student, do you like the class?";
+            }
+            saySomething();
+        ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -25,5 +25,6 @@
             echo "<br/>" . $number . "<br/>";
         }
     ?>
+    
 </body>
 </html>
