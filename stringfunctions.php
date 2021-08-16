1fe9db450fa0e57e37e87764cd30d0882f7ba45b
ltanksley10 <contactlthere@gmail.com>
2021-08-16 14:22:29 -0500

    string functions in php

---
+++ stringfunctions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>String functions</title>
+    </head>
+    <body>
+    
+        <?php
+
+            $string = "Hello student do you like the class";
+
+            echo strlen($string);
+
+            echo "<br>";
+
+            echo strtoupper($string);
+
+            echo "<br>";
+
+            echo strtolower($string);
+
+        ?>
+
+    </body>
+</html>
