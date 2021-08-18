b04e98052e592d6809b33b239f04c898d36a2b18
ltanksley10 <contactlthere@gmail.com>
2021-08-18 13:29:23 -0500

    form submissions external

---
+++ externalformsubmission.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>External form</title>
+    </head>
+    <body>
+        <form action="formprocesses.php" method="post">
+            <input type="text" name="username" placeholder="Enter Username">
+            <input type="password" name="password" placeholder="Enter Password">
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
--- formprocesses.php
+++ formprocesses.php
@@ -27,19 +27,3 @@
         // echo "your password is " . $password;
     }
 ?>
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Form Submissions</title>
-    </head>
-    <body>
-        <form action="formsubmissions.php" method="post">
-            <input type="text" name="username" placeholder="Enter Username">
-            <input type="password" name="password" placeholder="Enter Password">
-            <input type="submit" name="submit">
-        </form>
-
-    </body>
-</html>
